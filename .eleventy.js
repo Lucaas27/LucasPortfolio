99db0efd139a438bdfe298788aff23111f463d79
Lucas Gomes <lucasggomees@gmail.com>
2023-02-13 21:29:00 +0000

    Pagination completed

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,16 @@ module.exports = function (eleventyConfig) {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
 
+    // First Page is 1: Retarded array in contrast with zero based.
+    eleventyConfig.addNunjucksFilter("hashIt", function (array) {
+        const hash = {};
+        for (let i = 0; i < array.length; i++) {
+            hash[i + 1] = array[i];
+        }
+
+        return hash;
+    });
+
 
 
     // Where public facing files will be located and where I'm working from
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -94,3 +94,14 @@ sr.reveal('.portfolio-card', { interval: '450' });
 sr.reveal(`.form-container`, {
     origin: 'top',
 });
+
+// !RETURN TO THE POSITION AFTER REFRESH
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    const scrollpos = localStorage.getItem("scrollpos");
+    if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+window.onscroll = function (e) {
+    localStorage.setItem("scrollpos", window.scrollY);
+};
