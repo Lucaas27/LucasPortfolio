2fd099179561142e84488de2c4ed4bf6d150b16e
Lucas Gomes <lucasggomees@gmail.com>
2023-03-06 21:03:40 +0000

    Added Netlify form submission

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -200,3 +200,27 @@ function attachEventListener(openModal, closeButtons, overlay) {
 }
 
 attachEventListener(openModal, closeModal, overlay);
+
+
+
+
+//HANDLE FORM SUBMISSION
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+    })
+        .then(() => alert("Thank you for your submission"))
+        .catch((error) => alert(error));
+};
+
+document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmit);
